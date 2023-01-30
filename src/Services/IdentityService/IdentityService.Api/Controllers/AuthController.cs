bd8f464e8f9929c6760b92c0b71650f6957baced
Tahsin Tiryaki <tahsin.tiryaki@sgs.com>
2023-01-30 14:46:36 +0300

    identity Service completed.

---
+++ src/Services/IdentityService/IdentityService.Api/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using IdentityServer.Application.Models;
+using IdentityServer.Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IIdentityService identityService;
+
+        public AuthController(IIdentityService identityService)
+        {
+            this.identityService = identityService;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginRequestModel loginRequestModel)
+        {
+            var result = await identityService.Login(loginRequestModel);
+
+            return Ok(result);
+        }
+    }
+}
